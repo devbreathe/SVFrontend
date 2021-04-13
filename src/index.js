31450446f86ac90c9104908439c91dd7f50a29da
Cristian Vargas <cristian.vargas@qrvey.com>
2021-04-12 22:57:00 -0500

    Working in login styles and index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
+import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
 import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
